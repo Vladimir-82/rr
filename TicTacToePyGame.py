f5b7d4d0d835f9a51bd0de61c105e448d200f43c
PavelFihuryn <belarus19@tut.by>
2020-11-30 16:48:45 +0300

    Добавил немного комментариев

--- TicTacToePyGame.py
+++ TicTacToePyGame.py
@@ -14,7 +14,7 @@ WIDTH_WINDOW = 310
 # Cell
 HEIGHT_CELL = 90
 WIDTH_CELL = 90
-MERGE = 10
+MERGE = 10  # Merge between cells
 
 
 class Cell:
@@ -68,6 +68,7 @@ class Field:
     def draw_text_cell(self, row, col, text='Text', x=0, y=0):
         cell = Cell(row, col)
         self.draw_cell(cell.x, cell.y, GREEN)
+        # if x and y not entered, than parameters depends coordinate cell
         self.print_text(text, cell.x + 5 if x == 0 else x, cell.y + 20 if y == 0 else y)
 
     def draw_playing_field(self, field):
@@ -81,67 +82,75 @@ class Field:
                     self.draw_circle_cell(j, i)
                 else:
                     print(f'Field {field} is not draw')
-                    pygame.quit()  # Роняем приложение
+                    pygame.quit()  # Exception
 
     def print_text(self, text, x, y, height=30, color=RED):
         font = pygame.font.Font('comic.ttf', height)
         text_surface = font.render(text, True, color)
         self.gameScreen.blit(text_surface, dest=(x, y))
 
+    # Menu of choice game figure
     def choice_figure_draw(self):
         self.print_text('Choice figure:', 60, 40, color=BLUE)
         self.draw_circle_cell(1, 0)
         self.draw_cross_cell(1, 2)
         self.print_text('or', 130, 110, color=GREEN, height=50)
 
+    # Event of choice game figure
     def choice_figure(self):
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 if is_cursor_in_cell(1, 0):
                     self.display_clear()
                     pygame.display.update()
-                    return 'O', False, True  # Figure, choice_flag, who_flag
+                    return 'O', False, True  # figure, figure_flag, who_start_flag
                 elif is_cursor_in_cell(1, 2):
                     self.display_clear()
                     pygame.display.update()
                     return 'X', False, True
         return '', True, False
 
+    # Menu of choice who start
     def who_start_first_draw(self):
         self.print_text('Who start first?', 40, 30, color=WHITE)
         self.draw_text_cell(1, 0, 'Man', x=25, y=130)
-        self.draw_text_cell(1, 2, 'Comp', x=220)  # Проверить
+        self.draw_text_cell(1, 2, 'Comp', x=220)
         self.print_text('or', 130, 110, height=50, color=BLUE)
 
+    # Event of choice who start
     def who_start_first(self):
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 if is_cursor_in_cell(1, 0):
                     self.display_clear()
-                    return 'human', False, True
+                    return 'human', False, True  # gamer, who_start_flag, main_game_flag
                 elif is_cursor_in_cell(1, 2):
                     self.display_clear()
                     return 'computer', False, True
         return '', True, False
 
+    # Menu of return game
     def end_game_draw(self, win):
         self.print_text(f'Winn {win}', 50, 30)
         self.print_text('Do you want', 70, 80)
         self.print_text('play again?', 80, 110)
-        self.draw_text_cell(2, 1, 'Yes', x=130, y=230)  # Проверить
+        self.draw_text_cell(2, 1, 'Yes', x=130, y=230)
 
-    def end_game(self, li):
+    # Event of return game
+    def end_game(self, li):  # li - list of game field
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 if is_cursor_in_cell(2, 1):
                     self.display_clear()
+                    # end_game_flag, main_game_flag, new clear list of game field
                     return False, True, [" " for x in range(9)]
         return True, False, li
 
+    # Checking who is the winner
     def winner(self, board, figure, gamer):
         if tictactoe.is_win(board, figure):
             self.display_clear()
-            return gamer, False, True
+            return gamer, False, True  # winner, main_game_flag, figure_flag
         if ' ' not in board:
             self.display_clear()
             return 'friendship', False, True
@@ -154,18 +163,19 @@ def change_player(player):
 
 
 if __name__ == '__main__':
-    board = [" " for x in range(9)]
     # Flags
     figure = ''
     gamer = ''
     win = ''
-    figure_flag = True  # флаг выхода из меню выбора фигур
-    who_start_flag = False  # флаг выхода из меню выбора кто начинает
+    figure_flag = True
+    who_start_flag = False
     main_game_flag = False
     end_game_flag = False
-    # Цикл игры
-    run_game_flag = True  # флаг выхода из цикла игры
-    game = Field()
+    run_game_flag = True
+
+    board = [" " for x in range(9)]  # Create game field
+    game = Field()  # Create object of game field
+
     while run_game_flag:
         # Draws
         if figure_flag:
@@ -180,7 +190,6 @@ if __name__ == '__main__':
                 win, main_game_flag, end_game_flag = game.winner(board, figure, gamer)
                 figure = tictactoe.change_player(figure)
                 gamer = 'human'
-
         if end_game_flag:
             game.end_game_draw(win)
         # Events
@@ -210,5 +219,4 @@ if __name__ == '__main__':
             if end_game_flag:
                 end_game_flag, figure_flag, board = game.end_game(board)
         pygame.display.update()
-    # Выход из игры:
     pygame.quit()
